679ece005b879f1c74de06c9c55834aed8573cae
deepakvenkat <dipak2911@gmail.com>
2013-07-05 13:17:18 -0400

    corrected the test for array sum pair

--- spec/array_sum_pair_spec.rb
+++ spec/array_sum_pair_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe '#array_pair_sum' do
   it "should return the correct pairs" do
-    puts array_pair_sum([1, 2, 3, 4, 5], 6)
+    array_pair_sum([1, 2, 3, 4, 5], 6).count.should == 2
+    array_pair_sum([1, 2, 3, 4, 5], 6).should include([4, 2], [5, 1])
   end
 end
